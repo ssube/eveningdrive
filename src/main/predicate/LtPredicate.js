35c631761a75db2d7edfaf69c6277979e1840540
ssube <ssube@redbrickhealth.com>
2016-01-13 14:56:33 -0600

    Added remaining simple operators.

---
+++ src/main/predicate/LtPredicate.js
@@ -0,0 +1,18 @@
+import Predicate from './Predicate';
+import Template from '../server/Template';
+
+export default class LtPredicate extends Predicate {
+  constructor(opts) {
+    const {lh, rh} = opts;
+
+    super(opts);
+
+    this._lh = new Template(lh);
+    this._rh = new Template(rh);
+  }
+
+  test(data) {
+    const lh = this._lh.render(data), rh = this._rh.render(data);
+    return lh < rh;
+  }
+}
--- src/main/predicate/PredicateCompiler.js
+++ src/main/predicate/PredicateCompiler.js
@@ -1,6 +1,10 @@
 import AndPredicate from './AndPredicate';
 import EqPredicate from './EqPredicate';
 import GtPredicate from './GtPredicate';
+import GteqPredicate from './GteqPredicate';
+import LtPredicate from './LtPredicate';
+import LteqPredicate from './LteqPredicate';
+import NeqPredicate from './NeqPredicate';
 import OrPredicate from './OrPredicate';
 
 export default class PredicateCompiler {
@@ -15,6 +19,14 @@ export default class PredicateCompiler {
         return new EqPredicate(opts);
       case 'gt':
         return new GtPredicate(opts);
+      case 'gteq':
+        return new GteqPredicate(opts);
+      case 'lt':
+        return new LtPredicate(opts);
+      case 'lteq':
+        return new LteqPredicate(opts);
+      case 'neq':
+        return new NePredicate(opts);
       case 'or':
         return new OrPredicate({sub});
     }
